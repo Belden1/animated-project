20513fd161dacb7a168412e07450346a6a1ad7ed
Kristopher Hellsten <carvajalhellstenmail@gmail.com>
2024-01-10 19:39:30 +0100

    Waves + ref, states and destroy

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,37 @@
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 // import CLOUDS from 'vanta/src/vanta.clouds';
-import TOPOLOGY from 'vanta/src/vanta.topology';
+// import TOPOLOGY from 'vanta/src/vanta.topology';
+import WAVES from 'vanta/src/vanta.waves';
 
 function App() {
+	const [vantaEffect, setVantaEffect] = useState(null);
+	const myRef = useRef(null);
 	useEffect(() => {
-		TOPOLOGY({
-			el: '#vanta',
-			mouseControls: true,
-			touchControls: true,
-			gyroControls: false,
-			minHeight: 200.0,
-			minWidth: 200.0,
-			scale: 1.0,
-			scaleMobile: 1.0,
-			color: 0x1b9191,
-			backgroundColor: 0x101111
-		});
-	});
+		if (!vantaEffect) {
+			setVantaEffect(
+				WAVES({
+					el: myRef.current,
+					mouseControls: true,
+					touchControls: true,
+					gyroControls: false,
+					minHeight: 200.0,
+					minWidth: 200.0,
+					scale: 1.0,
+					scaleMobile: 1.0,
+					waveSpeed: 2.5
+				})
+			);
+		} else {
+			return () => {
+				if (vantaEffect) vantaEffect.destroy();
+			};
+		}
+	}, []);
 
 	return (
 		<main>
-			<div className="bg" id="vanta">
+			<div className="bg" ref={myRef}>
 				<h1>Cloudy Days</h1>
 				<p>
 					Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit perspiciatis delectus temporibus!
