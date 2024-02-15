0213ef15232fcd687affa61c7cfeb1ebac541da8
Kristopher Hellsten <carvajalhellstenmail@gmail.com>
2024-02-15 21:09:34 +0100

    Updating prettier

--- src/App.jsx
+++ src/App.jsx
@@ -5,41 +5,41 @@ import './App.css';
 import WAVES from 'vanta/src/vanta.waves';
 
 function App() {
-	const [vantaEffect, setVantaEffect] = useState(null);
-	const myRef = useRef(null);
-	useEffect(() => {
-		if (!vantaEffect) {
-			setVantaEffect(
-				WAVES({
-					el: myRef.current,
-					mouseControls: true,
-					touchControls: true,
-					gyroControls: false,
-					minHeight: 200.0,
-					minWidth: 200.0,
-					scale: 1.0,
-					scaleMobile: 1.0,
-					waveSpeed: 2.5
-				})
-			);
-		} else {
-			return () => {
-				if (vantaEffect) vantaEffect.destroy();
-			};
-		}
-	}, []);
+  const [vantaEffect, setVantaEffect] = useState(null);
+  const myRef = useRef(null);
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(
+        WAVES({
+          el: myRef.current,
+          mouseControls: true,
+          touchControls: true,
+          gyroControls: false,
+          minHeight: 200.0,
+          minWidth: 200.0,
+          scale: 1.0,
+          scaleMobile: 1.0,
+          waveSpeed: 2.5
+        })
+      );
+    } else {
+      return () => {
+        if (vantaEffect) vantaEffect.destroy();
+      };
+    }
+  }, []);
 
-	return (
-		<main>
-			<div className="bg" ref={myRef}>
-				<h1>Cloudy Days</h1>
-				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit perspiciatis delectus temporibus!
-					Quaerat beatae dicta animi at fugit doloribus amet eveniet accusamus? Nihil, officia alias.
-				</p>
-			</div>
-		</main>
-	);
+  return (
+    <main>
+      <div className="bg" ref={myRef}>
+        <h1>Cloudy Days</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit perspiciatis delectus temporibus! Quaerat
+          beatae dicta animi at fugit doloribus amet eveniet accusamus? Nihil, officia alias.
+        </p>
+      </div>
+    </main>
+  );
 }
 
 export default App;
